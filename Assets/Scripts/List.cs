199cb63c2c2c4b4a2f10eb24666ad44eb424639d
Tomaz Saraiva <tomaz.saraiva@gmail.com>
2017-05-02 23:12:55 +0100

    add scroll orientation add gallery scene add support for horizontal scrolling

--- Assets/Scripts/List.cs
+++ Assets/Scripts/List.cs
@@ -51,11 +51,18 @@ namespace AddComponent
 		#endregion
 
 
-		private enum Direction
+		private enum ScrollOrientation
 		{
-			DOWN,
-			UP
+			HORIZONTAL,
+			VERTICAL
 		}
+
+		private enum ScrollDirection
+		{
+			NEXT,
+			PREVIOUS
+		}
+
 
 		[SerializeField]
 		private ScrollRect _scrollRect;
@@ -67,18 +74,20 @@ namespace AddComponent
 		private RectTransform _content;
 
 		[SerializeField]
+		private ScrollOrientation _scrollOrientation;
+
+		[SerializeField]
 		private float _spacing;
 
 
-		private List<ListItemBase> _items;
+		private List<ListItemBase> _itemsList;
 
 		private int _itemsTotal;
-		private int _itemsVisible;
 
-		private int _itemsToRecycleTop;
-		private int _itemsToRecycleBottom;
+		private int _itemsToRecycleBefore;
+		private int _itemsToRecycleAfter;
 
-		private float _listItemHeight;
+		private Vector2 _listItemSize;
 
 		private float _lastPosition = -1;
 
@@ -87,28 +96,37 @@ namespace AddComponent
 
 		public void Create(int items, ListItemBase listItemPrefab)
 		{
-			_listItemHeight = listItemPrefab.Size.y;
-
-			_content.sizeDelta = new Vector2(_content.sizeDelta.x, _listItemHeight * items + _spacing * (items - 1));
-
-			_itemsTotal = items;
-			_itemsVisible = Mathf.FloorToInt (_viewport.rect.height / _listItemHeight) * 2;
-			_itemsToRecycleBottom = _itemsVisible / 2;
-
-			_currentIndex = _itemsVisible - 1;
-
-			_items = new List<ListItemBase> ();
-
-			for (int i = 0; i < _itemsVisible; i++)
+			switch (_scrollOrientation)
 			{
-				ListItemBase item = CreateNewItem (listItemPrefab, i * (_listItemHeight + _spacing) + _listItemHeight / 2);
-				item.onSelected = HandleOnSelectedHandler;
-				item.Index = i;
+			case ScrollOrientation.HORIZONTAL:
+				_scrollRect.vertical = false;
+				_scrollRect.horizontal = true;
 
-				_items.Add(item);
+				_content.anchorMin = new Vector2 (0, 0);
+				_content.anchorMax = new Vector2 (0, 1);
+				break;
 
-				ItemLoaded (item);
+			case ScrollOrientation.VERTICAL:
+				_scrollRect.vertical = true;
+				_scrollRect.horizontal = false;
+
+				_content.anchorMin = new Vector2 (0, 1);
+				_content.anchorMax = new Vector2 (1, 1);
+				break;
 			}
+
+			_content.sizeDelta = GetContentSize(items, listItemPrefab);
+
+			_itemsList = CreateItemsList (listItemPrefab);
+
+
+			_listItemSize = listItemPrefab.Size;
+			_itemsTotal = items;
+
+			_currentIndex = _itemsList.Count - 1;
+
+			_itemsToRecycleAfter = _itemsList.Count / 2;
+
 
 			_scrollRect.onValueChanged.AddListener ((Vector2 position) => 
 			{
@@ -120,15 +138,15 @@ namespace AddComponent
 		{
 			_scrollRect.verticalNormalizedPosition = 1;
 
-			if(_items != null)
+			if(_itemsList != null)
 			{
-				for (int i = 0; i < _items.Count; i++)
+				for (int i = 0; i < _itemsList.Count; i++)
 				{
-					Destroy (_items [i].gameObject);
+					Destroy (_itemsList [i].gameObject);
 				}
 
-				_items.Clear ();
-				_items = null;
+				_itemsList.Clear ();
+				_itemsList = null;
 			}
 
 			_lastPosition = -1;
@@ -141,17 +159,91 @@ namespace AddComponent
 		}
 
 
-		private ListItemBase CreateNewItem(ListItemBase prefab, float positionY)
+		private Vector2 GetContentSize(int items, ListItemBase prefab)
+		{
+			Vector2 contentSize = Vector2.zero;
+			float contentSizeMultiplier = items + _spacing * (items - 1);
+
+			switch (_scrollOrientation)
+			{
+			case ScrollOrientation.HORIZONTAL:
+				contentSize.x = prefab.Size.x * contentSizeMultiplier;
+				break;
+
+			case ScrollOrientation.VERTICAL:
+				contentSize.y = prefab.Size.y * contentSizeMultiplier;
+				break;
+			}
+
+			return contentSize;
+		}
+
+		private List<ListItemBase> CreateItemsList(ListItemBase prefab)
+		{
+			List<ListItemBase> itemsList = new List<ListItemBase> ();
+
+			float itemDimension = 0;
+			float viewportDimension = 0;
+
+			switch (_scrollOrientation)
+			{
+			case ScrollOrientation.HORIZONTAL:
+				viewportDimension = _viewport.rect.width;
+				itemDimension = prefab.Size.x;
+				break;
+
+			case ScrollOrientation.VERTICAL:
+				viewportDimension = _viewport.rect.height;
+				itemDimension = prefab.Size.y;
+				break;
+			}
+
+			int itemsToInstantiate = Mathf.FloorToInt (viewportDimension / itemDimension) * 2;
+
+			for (int i = 0; i < itemsToInstantiate; i++)
+			{
+				ListItemBase item = CreateNewItem (prefab, i, itemDimension);
+				item.onSelected = HandleOnSelectedHandler;
+				item.Index = i;
+
+				itemsList.Add(item);
+
+				ItemLoaded (item);
+			}
+
+			return itemsList;
+		}
+
+
+		private ListItemBase CreateNewItem(ListItemBase prefab, int index, float dimension)
 		{
 			GameObject instance = (GameObject)Instantiate (prefab.gameObject, Vector3.zero, Quaternion.identity);
 			instance.transform.SetParent (_content.transform);
 			instance.transform.localScale = Vector3.one;
 			instance.SetActive (true);
 
+			float position = index * (dimension + _spacing) + dimension / 2;
+
 			RectTransform rectTransform = instance.GetComponent <RectTransform> ();
-			rectTransform.anchoredPosition = new Vector2(0, -positionY);
-			rectTransform.offsetMin = new Vector2 (0, rectTransform.offsetMin.y);
-			rectTransform.offsetMax = new Vector2 (0, rectTransform.offsetMax.y);
+
+			switch (_scrollOrientation)
+			{
+			case ScrollOrientation.HORIZONTAL:
+				rectTransform.anchorMin = new Vector2 (0, 0);
+				rectTransform.anchorMax = new Vector2 (0, 1);
+				rectTransform.anchoredPosition = new Vector2 (position, 0);
+				rectTransform.offsetMin = new Vector2 (rectTransform.offsetMin.x, 0);
+				rectTransform.offsetMax = new Vector2 (rectTransform.offsetMax.x, 0);
+				break;
+
+			case ScrollOrientation.VERTICAL:
+				rectTransform.anchorMin = new Vector2 (0, 1);
+				rectTransform.anchorMax = new Vector2 (1, 1);
+				rectTransform.anchoredPosition = new Vector2 (0, -position);
+				rectTransform.offsetMin = new Vector2 (0, rectTransform.offsetMin.y);
+				rectTransform.offsetMax = new Vector2 (0, rectTransform.offsetMax.y);
+				break;
+			}
 
 			return instance.GetComponent <ListItemBase> ();
 		}
@@ -165,22 +257,22 @@ namespace AddComponent
 				return;
 			}
 
-			int rows = Mathf.FloorToInt (Mathf.Abs (_content.anchoredPosition.y - _lastPosition) / _listItemHeight);
+			int rows = Mathf.FloorToInt (Mathf.Abs (_content.anchoredPosition.y - _lastPosition) / _listItemSize.y);
 
 			if (rows == 0)
 			{
 				return;
 			}
 
-			Direction direction = _lastPosition > _content.anchoredPosition.y ? Direction.UP : Direction.DOWN;
+			ScrollDirection direction = _lastPosition > _content.anchoredPosition.y ? ScrollDirection.PREVIOUS : ScrollDirection.NEXT;
 
 			for (int i = 0; i < rows; i++)
 			{
 				switch (direction)
 				{
-				case Direction.DOWN:
+				case ScrollDirection.NEXT:
 
-					if (_itemsToRecycleTop > _itemsVisible / 4 && _currentIndex < _itemsTotal - 1)
+					if (_itemsToRecycleBefore > _itemsList.Count / 4 && _currentIndex < _itemsTotal - 1)
 					{
 						_currentIndex++;
 
@@ -188,17 +280,17 @@ namespace AddComponent
 					}
 					else
 					{
-						_itemsToRecycleTop++; 
-						_itemsToRecycleBottom--;
+						_itemsToRecycleBefore++; 
+						_itemsToRecycleAfter--;
 					}
 
-					_lastPosition += _listItemHeight + _spacing;
+					_lastPosition += _listItemSize.y + _spacing;
 
 					break;
 
-				case Direction.UP:
+				case ScrollDirection.PREVIOUS:
 
-					if (_itemsToRecycleBottom > _itemsVisible / 4 && _currentIndex > _itemsVisible - 1)
+					if (_itemsToRecycleAfter > _itemsList.Count / 4 && _currentIndex > _itemsList.Count - 1)
 					{
 						RecycleItem (direction);
 
@@ -206,44 +298,44 @@ namespace AddComponent
 					}
 					else
 					{
-						_itemsToRecycleTop--; 
-						_itemsToRecycleBottom++;
+						_itemsToRecycleBefore--; 
+						_itemsToRecycleAfter++;
 					}
 
-					_lastPosition -= _listItemHeight + _spacing;
+					_lastPosition -= _listItemSize.y + _spacing;
 
 					break;
 				}
 			}
 		}
 
-		private void RecycleItem(Direction direction)
+		private void RecycleItem(ScrollDirection direction)
 		{
-			ListItemBase firstItem = _items [0];
-			ListItemBase lastItem = _items [_items.Count - 1];
+			ListItemBase firstItem = _itemsList [0];
+			ListItemBase lastItem = _itemsList [_itemsList.Count - 1];
 
-			float targetPositionY = (_listItemHeight + _spacing) * -1;
+			float targetPositionY = (_listItemSize.y + _spacing) * -1;
 
 			switch(direction)
 			{
-			case Direction.DOWN:
+			case ScrollDirection.NEXT:
 				firstItem.Position = new Vector2 (firstItem.Position.x, lastItem.Position.y + targetPositionY);
 				firstItem.Index = _currentIndex;
 				firstItem.transform.SetAsLastSibling ();
 
-				_items.RemoveAt (0);
-				_items.Add (firstItem);
+				_itemsList.RemoveAt (0);
+				_itemsList.Add (firstItem);
 
 				ItemLoaded (firstItem);
 				break;
 
-			case Direction.UP:
+			case ScrollDirection.PREVIOUS:
 				lastItem.Position = new Vector2 (lastItem.Position.x, firstItem.Position.y - targetPositionY);
-				lastItem.Index = _currentIndex - _itemsVisible;
+				lastItem.Index = _currentIndex - _itemsList.Count;
 				lastItem.transform.SetAsFirstSibling ();
 
-				_items.RemoveAt (_items.Count - 1);
-				_items.Insert (0, lastItem);
+				_itemsList.RemoveAt (_itemsList.Count - 1);
+				_itemsList.Insert (0, lastItem);
 
 				ItemLoaded (lastItem);
 				break;
